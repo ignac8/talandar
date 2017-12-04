9b5996676a69b95f286bfe24c161bd21263c4a34
ignac8 <ignac8@op.pl>
2017-12-04 20:20:30 +0100

    Wip

--- src/test/FitnessEvaluatorTests.java
+++ src/test/FitnessEvaluatorTests.java
@@ -22,14 +22,6 @@ public class FitnessEvaluatorTests {
 
     @Test
     public void consistentResultsTestForNeuralNetworkPlayer() {
-        boolean graphics = false;
-        double simulationTimeStep = 1.0;
-        double simulationTimeLimit = Double.MAX_VALUE;
-        double mapHeight = 640.0;
-        double mapWidth = 640.0;
-        double gapHeight = 40.0;
-        double gapWidth = 120.0;
-        int numberOfUnitSelections = 10;
         int inputLayerSize = 5;
         int outputLayerSize = 15;
         double std = 1000;
@@ -38,17 +30,28 @@ public class FitnessEvaluatorTests {
 
         NeuralNetwork neuralNetwork = new FCFSNeuralNetwork(inputLayerSize, hiddenLayerSizes, outputLayerSize, std, mean);
 
-        List<FitnessEvaluator> fitnessEvaluators = new ArrayList<>();
-
-        List<Pair<List<List<UnitType>>, List<List<UnitType>>>> unitSelections
-                = generateRandomUnitSelections(numberOfUnitSelections);
-
-        unitSelections.addAll(UnitSelectionGenerator.generateMirrorUnitSelections(unitSelections));
-
         Player firstPlayer = new NeuralNetworkPlayer(0);
-        Player secondPlayer = new SimplePlayer(1);
         NeuralNetworkPlayer neuralNetworkPlayer = (NeuralNetworkPlayer) (firstPlayer);
         neuralNetworkPlayer.setNeuralNetwork(neuralNetwork);
+
+        testAgainstSimplePlayer(firstPlayer);
+    }
+
+    private void testAgainstSimplePlayer(Player firstPlayer) {
+        boolean graphics = false;
+        double simulationTimeStep = 1.0;
+        double simulationTimeLimit = Double.MAX_VALUE;
+        double mapHeight = 640.0;
+        double mapWidth = 640.0;
+        double gapHeight = 40.0;
+        double gapWidth = 120.0;
+        int numberOfUnitSelections = 10;
+
+        List<FitnessEvaluator> fitnessEvaluators = new ArrayList<>();
+        List<Pair<List<List<UnitType>>, List<List<UnitType>>>> unitSelections
+                = generateRandomUnitSelections(numberOfUnitSelections);
+        unitSelections.addAll(UnitSelectionGenerator.generateMirrorUnitSelections(unitSelections));
+        Player secondPlayer = new SimplePlayer(1);
 
         for (Pair<List<List<UnitType>>, List<List<UnitType>>> unitSelection : unitSelections) {
             SimulationEvaluator fitnessEvaluator = new SimulationEvaluator(graphics, simulationTimeStep,
@@ -69,39 +72,7 @@ public class FitnessEvaluatorTests {
 
     @Test
     public void consistentResultsTestForSimplePlayer() {
-        boolean graphics = false;
-        double simulationTimeStep = 1.0;
-        double simulationTimeLimit = Double.MAX_VALUE;
-        double mapHeight = 640.0;
-        double mapWidth = 640.0;
-        double gapHeight = 40.0;
-        double gapWidth = 120.0;
-        int numberOfUnitSelections = 10;
-
-        List<FitnessEvaluator> fitnessEvaluators = new ArrayList<>();
-
-        List<Pair<List<List<UnitType>>, List<List<UnitType>>>> unitSelections
-                = generateRandomUnitSelections(numberOfUnitSelections);
-
-        unitSelections.addAll(UnitSelectionGenerator.generateMirrorUnitSelections(unitSelections));
-
         Player firstPlayer = new SimplePlayer(0);
-        Player secondPlayer = new SimplePlayer(1);
-
-        for (Pair<List<List<UnitType>>, List<List<UnitType>>> unitSelection : unitSelections) {
-            SimulationEvaluator fitnessEvaluator = new SimulationEvaluator(graphics, simulationTimeStep,
-                    simulationTimeLimit, mapHeight, mapWidth, gapHeight, gapWidth, firstPlayer, secondPlayer);
-            fitnessEvaluator.setUnitSelection(unitSelection);
-            fitnessEvaluators.add(fitnessEvaluator);
-        }
-
-        for (FitnessEvaluator fitnessEvaluator : fitnessEvaluators) {
-            double previousFitness = fitnessEvaluator.evaluate();
-            for (int counter = 0; counter < 1000; counter++) {
-                double currentFitness = fitnessEvaluator.evaluate();
-                assertThat(currentFitness, is(previousFitness));
-                previousFitness = currentFitness;
-            }
-        }
+        testAgainstSimplePlayer(firstPlayer);
     }
 }
